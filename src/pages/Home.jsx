55da91b8a81aeb6e3a0e31dd24fbd218c4189b70
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-04-16 10:14:20 +0530

    add products card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import Image1 from "../assets/";
 
 function Home() {
+  // best Sellers array
+  const bestSellers = [
+    {
+      id: 1,
+      name: "Handmade Dreamcatcher",
+      price: "2,500",
+      artist: "Nimali Apsara",
+      image: "Image1",
+    },
+    {
+      id: 2,
+      name: "Clay Flower Vase",
+      price: "1,800",
+      artist: "Nimali Apsara",
+      image: "Image1",
+    },
+    {
+      id: 3,
+      name: "Wooden Keytag",
+      price: "450",
+      artist: "Nimali Apsara",
+      image: "Image1",
+    },
+    {
+      id: 4,
+      name: "Hand-painted Tote Bag",
+      price: "2,000",
+      artist: "Nimali Apsara",
+      image: "Image1",
+    },
+  ];
+
   return (
+    // home section
     <div className="home">
       <section className="hero">
         <div className="hero__content">
@@ -13,6 +48,23 @@ function Home() {
           <button type="button" className="hero__btn">
             View All Crafts
           </button>
+        </div>
+      </section>
+
+      {/* best sellers section */}
+      <section className="best-sellers">
+        <h2>Best Sellers</h2>
+
+        <div className="product-grid">
+          {bestSellers.map((product) => (
+            <ProductCard
+              key={product.id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              artist={product.artist}
+            />
+          ))}
         </div>
       </section>
     </div>
