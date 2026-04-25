e7e0613cc7e20bca2d28bcf64ab1456bd2baf772
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-04-25 12:15:23 +0530

    feat: add products details with images and attributes

---
+++ src/data/productsDetails.js
@@ -0,0 +1,76 @@
+import Image1 from "../assets/dreamcatchers.jpg";
+import Image2 from "../assets/clay-pot.jpg";
+import Image3 from "../assets/key-tags.jpg";
+import Image4 from "../assets/bags.jpg";
+
+// src/data/products.js
+export const allProducts = [
+  {
+    id: 1,
+    name: "Traditional Mask",
+    price: 4500,
+    artist: "Gunapala",
+    category: "Art",
+    image: "/assets/img1.jpg",
+  },
+  {
+    id: 2,
+    name: "Clay Pot Set",
+    price: 2200,
+    artist: "Soma",
+    category: "Home Decor",
+    image: "/assets/img2.jpg",
+  },
+  {
+    id: 3,
+    name: "Handmade Necklace",
+    price: 1500,
+    artist: "Nimali",
+    category: "Accessories",
+    image: "/assets/img3.jpg",
+  },
+  {
+    id: 4,
+    name: "Woven Basket",
+    price: 3000,
+    artist: "Kamal",
+    category: "Home Decor",
+    image: "/assets/img4.jpg",
+  },
+  {
+    id: 5,
+    name: "Wooden Keytag",
+    price: 450,
+    artist: "Siripala",
+    category: "Accessories",
+    image: "/assets/img5.jpg",
+  },
+  {
+    id: 5,
+    name: "Handmade Dreamcatcher",
+    price: "2,500",
+    artist: "Nimali Apsara",
+    image: Image1,
+  },
+  {
+    id: 6,
+    name: "Clay Flower Vase",
+    price: "1,800",
+    artist: "Saman Kumara",
+    image: Image2,
+  },
+  {
+    id: 7,
+    name: "Wooden Keytag",
+    price: "450",
+    artist: "Nethmi Pabasara",
+    image: Image3,
+  },
+  {
+    id: 8,
+    name: "Hand-painted Tote Bag",
+    price: "2,000",
+    artist: "Sadun Ekanayake",
+    image: Image4,
+  },
+];
